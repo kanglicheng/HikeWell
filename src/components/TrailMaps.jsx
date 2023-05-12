e09b8018a655eb86c06ef17a7a2576a5cdcf601c
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-05-12 09:56:00 -0400

    add insert for TrailMaps

--- src/components/TrailMaps.jsx
+++ src/components/TrailMaps.jsx
@@ -6,6 +6,20 @@ export const TrailMaps = () => {
       <h3>Maps Table</h3>
 
       <button>View All TrailMaps</button>
+
+      <div>
+        <label>Add TrailMap</label>
+        <form>
+          <label>Trail</label>
+          <select>
+            <option>Oregon Trail</option>
+          </select>
+          <label>Map</label>
+          <select>
+            <option>Northwest US</option>
+          </select>
+        </form>
+      </div>
     </div>
   );
 };
