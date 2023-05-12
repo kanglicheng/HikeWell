9618034fd077b27f03eefb18164c1fc93e37c83e
kanglicheng <11033065+kanglicheng@users.noreply.github.com>
2023-05-11 22:53:46 -0400

    add mock update for trail

--- src/components/Trails.jsx
+++ src/components/Trails.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 export const Trais = () => {
+  const [showForm, setShowForm] = React.useState(false);
+  const [selectedTrail, setSelectedTrail] = React.useState("");
+
+  const handleSelection = (e) => {
+    setSelectedTrail(e.target.value);
+    setShowForm(true);
+  };
+
   return (
     <div>
       <h3>Trails Table</h3>
@@ -13,26 +21,63 @@ export const Trais = () => {
         </label>
         <form>
           <div>
-            <label>Name</label>
+            <label>Name </label>
             <input type="text" />
-            <label>City</label>
+            <label>City </label>
             <input type="text" />
           </div>
           <div>
-            <label>State</label>
+            <label> State </label>
             <input type="text" />
-            <label>Latitude</label>
+            <label> Latitude </label>
             <input type="number" />
-            <label>Longitude</label>
+            <label> Longitude </label>
             <input type="number" />
-            <label>Distance</label>
+            <label> Distance </label>
             <input type="number" />
           </div>
           <div style={{ margin: "10px" }}>
-            <button>Add Trail </button>
+            <button> Add Trail </button>
           </div>
         </form>
       </div>
+
+      <div>
+        <label>Update a Trail</label>
+        <select onChange={handleSelection}>
+          <option>Mt Wilson</option>
+          <option>Moose Mountain</option>
+        </select>
+      </div>
+      {showForm && (
+        <div
+          style={{
+            padding: "5px",
+            margin: "20px",
+            border: "1px solid magenta",
+          }}
+        >
+          <form>
+            <div>
+              <label>Name</label>
+              <input type="text" placeholder={selectedTrail} />
+              <label>City</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>State</label>
+              <input type="text" />
+              <label>Latitude</label>
+              <input type="number" />
+              <label>Longitude</label>
+              <input type="number" />
+              <label>Distance</label>
+              <input type="number" />
+            </div>
+            <button onClick={() => setShowForm(false)}>Update Trail</button>
+          </form>
+        </div>
+      )}
 
       <div>
         <label>Delete a Trail</label>
